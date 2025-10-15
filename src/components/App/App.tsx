ab941acd7dffede9455b4680e034c1e5f501351b
Nataliia Bielik <natalia.belic2607@gmail.com>
2025-10-15 08:34:17 -0400

    add pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import toast, { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SeearchBar from "../SearchBar/SeearchBar";
 import styles from "./App.module.css";
 import type { Movie } from "../../types/movie";
@@ -8,41 +8,67 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [query, setQuery] = useState<string>("")
+  const [page, setPage] = useState<number>(1)
 
-  const handleSearch = async (query: string) => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      setMovies([]);
-      const fetchedMovies = await fetchMovies(query);
-      if (fetchedMovies.length === 0) {
-        toast.error("no movies found for your request");
-        return;
-      }
-      setMovies(fetchedMovies);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const { data, isPending, isError, isSuccess } = useQuery({
+    queryKey: ["movies", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
+  });
+
+  const handleSearch = (newQuery: string) => {
+    setPage(1)
+    setQuery(newQuery);
   };
 
   const handleSelectMovie = (movie: Movie | null) => {
    setSelectedMovie(movie)
- }
+  }
+  
+  useEffect(() => {
+
+    if (isSuccess && data.results.length === 0) {
+  toast.error("No movies found for your request.");
+}
+
+
+  }, [data?.results.length, isSuccess])  
+
+const totalPages = data?.total_pages ?? 0
+
   return (
     <div className={styles.app}>
-      
       <SeearchBar onSubmit={handleSearch} />
-      {isLoading && <Loader />}
+      {isSuccess && totalPages > 1 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setPage(selected + 1)}
+          forcePage={page - 1}
+          containerClassName={styles.pagination}
+          activeClassName={styles.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
+      {query !== "" && isPending && <Loader />}
       {isError && <ErrorMessage />}
-      {movies.length > 0 && <MovieGrid movies={movies} onSelect={handleSelectMovie} />}
-      {selectedMovie && <MovieModal movie={selectedMovie} onClose={()=>handleSelectMovie(null) } /> }
+      {isSuccess && data.results.length > 0 && (
+        <MovieGrid movies={data.results} onSelect={handleSelectMovie} />
+      )}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => handleSelectMovie(null)}
+        />
+      )}
       <Toaster />
     </div>
   );
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,17 +3,20 @@ import type { Movie } from "../types/movie";
 
 interface TMDBRespons {
   results: Movie[];
-  page: number;
+  
   total_pages: number;
 }
 
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<TMDBRespons> => {
   const response = await axios.get<TMDBRespons>(
     "https://api.themoviedb.org/3/search/movie",
     {
-      params: { query },
+      params: { query, page },
       headers: { Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}` },
     }
-    );
-    return response.data.results
+  );
+  return response.data;
 };
